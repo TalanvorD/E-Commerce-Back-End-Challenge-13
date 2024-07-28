fbecfe3eb1a8c3243be884fcbf9faa27808bfb26
TalanvorD <164896317+TalanvorD@users.noreply.github.com>
2024-07-28 06:50:30 -0500

    Some updates.
    dotenv added, server configured, models built out, basic get all routes working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,20 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({ include: [{ model: Tag }, { model: Category }] });
+    if (!productData) {
+      res.status(404).json({ message: 'No products found!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
 
 // get one product
